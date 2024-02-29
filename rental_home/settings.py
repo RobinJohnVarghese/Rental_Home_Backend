62f272054b261655f187e6c89b8a4a84b29efa2b
Robin John Varghese <robinvarghesejohn1998@gmail.com>
2024-02-29 19:58:53 +0530

    Updated CHANNEL_LAYERS

--- rental_home/settings.py
+++ rental_home/settings.py
@@ -56,10 +56,14 @@ INSTALLED_APPS = [
 ]
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+    'default': {
+       'BACKEND': 'channels_redis.core.RedisChannelLayer',
+       'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
+
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
